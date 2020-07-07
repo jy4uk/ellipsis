518ecac88a40bbf8f2d5da14fb7ab2497829c59f
James Yang <jy4uk@virginia.edu>
2020-07-08 09:14:13 +1000

    rename title

--- index.php
+++ index.php
@@ -26,7 +26,7 @@ if ( isset( $_SESSION['user']) ) {
 -->
 <html>
 	<head>
-		<title>Momma is the Best!</title>
+		<title>Ellipsis</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
 		<link rel="stylesheet" href="assets/css/main.css" />
